3e897f3412e958d0ccd7ba3e5f6f47eb21621952
Yizzy Wu <wut343@newschool.edu>
2016-05-12 02:45:24 -0400

    add keys interactions

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,67 +1,3 @@
-/*jslint browser: true, plusplus: true */
-/*global window, document */
-
-window.onload = function () {
-    'use strict';
-
-    var body = document.querySelector('body'),
-        firstChildOfBody = body.firstElementChild,
-        gridLayer = document.createElement('div'),
-        gridChoice = 0;
-
-    gridLayer.setAttribute('id', 'column-baseline-grid');
-
-    if (null !== firstChildOfBody) {
-        body.insertBefore(gridLayer, firstChildOfBody);
-    } else {
-        body.textContent = 'The body element does not have a child element.';
-    }
-
-    document.onkeydown = function (evnt) {
-        if (27 === evnt.keyCode) {
-            switch (gridChoice) {
-            case 0:
-                gridLayer.classList.add('column-grid');
-                gridLayer.classList.remove('user-supplied-bg-image');
-
-                break;
-
-            case 1:
-                gridLayer.classList.remove('column-grid');
-                gridLayer.classList.add('modular-grid');
-
-                break;
-
-            case 2:
-                gridLayer.classList.remove('modular-grid');
-                gridLayer.classList.add('baseline-grid');
-
-                break;
-
-            case 3:
-                gridLayer.classList.remove('baseline-grid');
-                gridLayer.classList.add('all-grids');
-
-                break;
-
-            case 4:
-                gridLayer.classList.remove('all-grids');
-                gridLayer.classList.add('user-supplied-bg-image');
-
-                break;
-
-            case 5:
-                gridLayer.classList.remove('user-supplied-bg-image');
-                break;
-            }
-
-            if (gridChoice++ === 5) {
-                gridChoice = 0;
-            }
-        }
-    };
-};
-
 /* jslint browser: true, devel: true, maxlen: 85 */
 /* global window */
 
@@ -179,32 +115,60 @@ window.onload = function() {
     var img1 = document.getElementById('img_1');
     var img2 = document.getElementById('img_2');
     var img3 = document.getElementById('img_3');
+    var img4 = document.getElementById('img_4');
+    var img5 = document.getElementById('img_5');
+    var img6 = document.getElementById('img_6');
+    var img7 = document.getElementById('img_7');
 
 
     function keyImgs_in(e) {
         var x = e.keyCode;
-        if (x == 72) {  // 72 is the H key
+        if (x == 76) {  // 76 is the L key
             img1.style.opacity = '1';
         }  
-        if (x == 17) {  // 27 is the ctrl key
+        if (x == 66) {  // 66 is the B key
             img2.style.opacity = '1';
         }  
-        if (x == 65) {  // 27 is the a key
-            img3.style.opacity = '1';
-        }      
+        if (x == 75) {  // 75 is the K key
+            img3.style.opacity = '0.9';
+        }
+        if (x == 68) {  // 68 is the D key
+            img4.style.opacity = '0.9';
+        }
+        if (x == 72) {  // 72 is the H key
+            img5.style.opacity = '0.9';
+        } 
+        if (x == 89) {  // 89 is the Y key
+            img6.style.opacity = '0.9';
+        }
+        if (x == 69) {  // 69 is the E key
+            img7.style.opacity = '0.9';
+        }
     }
 
     function keyImgs_out(e) {
         var x = e.keyCode;
-        if (x == 72) {  // 72 is the H key
+        if (x == 76) {  // 76 is the L key
             img1.style.opacity = '0';
         }
-        if (x == 17) {  // 27 is the ctrl key
+        if (x == 66) {  // 66 is the B key
             img2.style.opacity = '0'; 
         }   
-        if (x == 65) {  // 27 is the ctrl key
+        if (x == 75) {  // 75 is the K key
             img3.style.opacity = '0'; 
-        }   
+        }
+        if (x == 68) {  // 68 is the D key
+            img4.style.opacity = '0';
+        }
+        if (x == 72) {  // 72 is the H key
+            img5.style.opacity = '0';
+        }    
+        if (x == 89) {  // 89 is the Y key
+            img6.style.opacity = '0';
+        }  
+        if (x == 69) {  // 69 is the E key
+            img7.style.opacity = '0';
+        }        
     }
 
     window.addEventListener ('keydown', keyImgs_in, false); 
